06ea47ced00d605df53da894c14ea2d162f17ee5
Brian Deane <bdeanemachine@gmail.com>
2014-09-03 03:39:34 +0000

    Yo dawg I heard you like tests

--- lib/bspec.rb
+++ lib/bspec.rb
@@ -1,2 +1,17 @@
 module BSpec
+  class TestObject
+    attr_reader :expression
+    def initialize(expression)
+      @expression = expression
+    end
+
+    def to_be(value)
+      raise "expected #{value} but got #{expression}" unless expression == value
+      true
+    end
+  end
+
+  def want(expression)
+    TestObject.new(expression)
+  end
 end
--- spec/bspec_spec.rb
+++ spec/bspec_spec.rb
@@ -1,8 +1,42 @@
 require 'bspec'
+include BSpec
+
+describe BSpec::TestObject do
+  it 'loads correctly' do
+    expect(BSpec::TestObject).to be_a(Class)
+  end
+
+  it 'raises exceptions with false comparisons' do
+    obj = BSpec::TestObject.new(3)
+    expect { obj.to_be(4) }.to raise_error
+  end
+
+  it 'returns true on true comparisons' do
+    obj = BSpec::TestObject.new(3)
+    expect(obj.to_be(3)).to be true
+  end
+end
 
 describe BSpec do
-  it 'correctly loads' do
+  it 'loads correctly' do
     expect(BSpec).to be_a(Module)
+  end
+
+  it 'returns a TestObject' do
+    expect(want('string')).to be_a(BSpec::TestObject)
+  end
+
+  it 'understands when things are true' do
+    true_exp = want(true).to_be true
+    expect(true_exp).to be true
+
+    true_exp = want(3).to_be 3
+    expect(true_exp).to be true
+  end
+
+  it 'understands when things are false' do
+    expect { want(true).to_be false }.to raise_error
+    expect { want(3).to_be 4 }.to raise_error
   end
 end
 
